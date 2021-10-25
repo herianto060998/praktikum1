338fb2872515bb184daf6146f7a97983276bf2b2
heri anto simamora <heriantosimamora6@gmail.com>
2021-10-25 14:49:39 +0700

    perubahan

--- tugaslatihan.py
+++ tugaslatihan.py
@@ -2,7 +2,7 @@ print=("=====TUGAS====")
 print=("=====MENGINPUT BIODATA DIRI=====")
 
 nama=input("nama:")
-call=input("panggilan:")
+panggilan=input("panggilan:")
 npm=input("npm:")
 ttl=input("ttl:")
 umur=input("umur:")
@@ -10,8 +10,8 @@ telepon=input("telepon:")
 alamat=input("alamat:")
 
 print ("assalamu'alaikum Wr.Wb")
-print ("Let me introduce my self. My name is ",nama,", but you can call me ",call,"."
-"My NPM is ",npm,". i was born in ",ttl," and I am ",umur," years old. i am"
-"very glad if you want to invite my house in ",alamat,". so, don't forget to call me"
-"before with the number ",telepon,".")
+print ("Let me introduce my self. My name is ",nama,", but you can call me ",panggilan,"."
+"My NPM is ",npm,". i was born in ",ttl," and I am ",umur," years old. "
+"i am very glad if you want to invite my house in ",alamat,"."
+"so, don't forget to call me before with the number ",telepon,".")
 print ("thank you")
