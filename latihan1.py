81338ddf7c2393d94f90ccafe8daefd0b68827b0
heri anto simamora <heriantosimamora6@gmail.com>
2021-10-25 14:28:25 +0700

    perubahan

---
+++ latihan1.py
@@ -0,0 +1,20 @@
+print("hello sayang")
+print("saya sedang belajar python")
+
+a= 8
+b= 6
+print ("variabel a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a+b=",a+b)
+
+a=input("msaukkan nilai a:")
+b=input("masukkan nilai b:")
+print("variabel a=",a)
+print("variabel b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+#konvensi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
