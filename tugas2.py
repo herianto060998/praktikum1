2b558054aa60f62ae2a9dc59d92838493cac1ede
heri anto simamora <heriantosimamora6@gmail.com>
2021-10-25 13:52:41 +0700

    perubahan file

---
+++ tugas2.py
@@ -0,0 +1,18 @@
+print=("=====TUGAS====")
+print=("=====MENGINPUT BIODATA DIRI=====")
+
+nama=input("nama:")
+call=input("panggilan:")
+npm=input("npm:")
+ttl=input("ttl:")
+umur=input("umur:")
+telepon=input("telepon:")
+alamat=input("alamat:")
+
+print ("assalamu'alaikum.")
+
+print ("Let me introduce my self. My name is ",nama,", but you can call me ",call,"."
+"My npm is ",npm,". i was born in ",ttl," and i am ",umur,"years old. "
+"i am very glad if you want to invite my  house in ",alamat,""
+". so, don't forget to call me before with the number ",telepon,". ")
+print ("thank you")
