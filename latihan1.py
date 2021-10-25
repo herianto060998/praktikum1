9431dbc996d09c4d937bd03d2a8e02343c5872d7
heri anto simamora <heriantosimamora6@gmail.com>
2021-10-25 18:57:19 +0700

    perubahan

--- latihan1.py
+++ latihan1.py
@@ -7,7 +7,7 @@ print ("variabel a=",a)
 print ("variable b=",b)
 print ("hasil penjumlahan a+b=",a+b)
 
-a=input("msaukkan nilai a:")
+a=input("masukkan nilai a:")
 b=input("masukkan nilai b:")
 print("variabel a=",a)
 print("variabel b=",b)
--- tugaslatihan.py
+++ tugaslatihan.py
@@ -1,17 +1,18 @@
 print=("=====TUGAS====")
 print=("=====MENGINPUT BIODATA DIRI=====")
 
-nama=input("nama:")
-panggilan=input("panggilan:")
-npm=input("npm:")
-ttl=input("ttl:")
-umur=input("umur:")
-telepon=input("telepon:")
-alamat=input("alamat:")
+nama=input ("nama:")
+panggilan=input ("panggilan:")
+npm=input ("npm:")
+ttl=input ("ttl:")
+umur=input ("umur:")
+alamat=input ("alamat:")
+telepon=input ("telepon:")
 
-print ("assalamu'alaikum Wr.Wb")
-print ("Let me introduce my self. My name is ",nama,", but you can call me ",panggilan,"."
-"My NPM is ",npm,". i was born in ",ttl," and I am ",umur," years old. "
-"i am very glad if you want to invite my house in ",alamat,"."
-"so, don't forget to call me before with the number ",telepon,".")
+print (" assalamu'alaikum Wr.Wb")
+print ("Let me introduce my self. My Name is ",nama,", but you can call me ",panggilan,"."
+"My NPM is ",npm,". i was born in ",ttl,"and i am",umur,"years old. "
+"i am very glad if you want to inivite my house in ",alamat,""
+".so, don't forget to call me before with the number ",telepon,".")
+
 print ("thank you")
